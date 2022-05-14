ab6297ad125bb751c8aba8bb9ef09ec7c23d2744
wdavidlopez4 <wdavidlopez4@gmail.com>
2022-05-14 16:59:30 -0500

    dominio completo

---
+++ src/Abogado.Domain/Ports/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Abogado.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abogado.Domain.Ports
+{
+    public interface IRepository
+    {
+        public Task Save<T>(T obj) where T : Entity;
+
+        public Task Update<T>(T obj) where T : Entity;
+
+        public T Exists<T>(Expression<Func<bool, T>> expression) where T : Entity;
+
+        public Task Commit<T>() where T : Entity;
+
+        public Task<T> Get<T>(Expression<Func<bool, T>> expression) where T : Entity; 
+    }
+}
