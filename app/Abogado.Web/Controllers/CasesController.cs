e385e2539135169354fe37affa56542602bd4441
cubides107 <cristian.cubides@uptc.edu.co>
2022-05-28 17:11:17 -0500

    F-Creacion de vistas para caso, falta implementacion para obtener los casos

---
+++ app/Abogado.Web/Controllers/CasesController.cs
@@ -0,0 +1,72 @@
+﻿using Abogado.Application.CasesServices.CreateCase;
+using Abogado.Application.CasesServices.ModifyCase;
+using Abogado.Web.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abogado.Web.Controllers
+{
+    [Authorize]
+    public class CasesController : Controller
+    {
+        private readonly IMediator mediator;
+
+        public CasesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "abogado")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "abogado")]
+        public IActionResult RegisterCase(RegisterCaseVM registerCase)
+        {
+            CreateCaseCommand command = new()
+            {
+                CaseName = registerCase.CaseName,
+                Description = registerCase.Description,
+                Trial = registerCase.Trial,
+                DivorceForm = registerCase.DivorceForm,
+                DivorceMechanism = registerCase.DivorceMechanism,
+                Archivo = registerCase.Archivo,
+                StartDate = registerCase.StartDate,
+            };
+
+            mediator.Send(command);
+
+            return View("Index");
+        }
+
+        [Authorize(Roles = "abogado")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "abogado")]
+        public IActionResult Edit(EditCaseVM editCase)
+        {
+            ModifyCaseCommand command = new()
+            {
+                CaseName = editCase.CaseName,
+                Description = editCase.Description,
+                Archivo = editCase.Archivo,
+            };
+
+            mediator.Send(command);
+            return View("Index");
+        }
+
+    }
+}
---
+++ app/Abogado.Web/Models/RegisterCaseVM.cs
@@ -0,0 +1,21 @@
+﻿using Abogado.Domain.Enums;
+
+namespace Abogado.Web.Models
+{
+    public class RegisterCaseVM
+    {
+        public string CaseName { get; set; }
+
+        public string Description { get; set; }
+
+        public Trial Trial { get; set; }
+
+        public DivorceForm DivorceForm { get; set; }
+
+        public DivorceMechanism DivorceMechanism { get; set; }
+
+        public IFormFile Archivo { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
